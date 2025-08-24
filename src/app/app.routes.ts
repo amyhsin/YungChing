5449801a6dbb5a56dd624edc82182163e86c882c
Hsin Wu <hsin.wu@wpgholdings.com>
2025-08-24 23:06:25 +0800

    create routes and two components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Attractions } from './pages/attractions/attractions';
+import { Favorites } from './pages/favorites/favorites';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'attractions', component: Attractions },
+    { path: 'favorites', component: Favorites },
+    { path: '', redirectTo: 'attractions', pathMatch: 'full' },
+];
